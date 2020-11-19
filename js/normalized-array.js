d29c25c09c3eb326a6262e3d7dcd6298cd1f70a6
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-11-19 17:56:17 +0500

    Add img of movies

--- js/normalized-array.js
+++ js/normalized-array.js
@@ -5,6 +5,7 @@ var normalizedMovies = movies.map(function (movie, i) {
       year: movie.movie_year,
       categories: movie.Categories.split('|'),
       summary: movie.summary,
+      img: `http://i3.ytimg.com/vi/${movie.ytid}/hqdefault.jpg`,
       imdbId: movie.imdb_id,
       imdbRating: movie.imdb_rating,
       language: movie.language,
